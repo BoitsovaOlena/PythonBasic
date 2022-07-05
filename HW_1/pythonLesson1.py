860e380eaa3be5674617243e7ed12db4891aa55f
Olena <elena.cerg@gmail.com>
2022-07-05 10:40:32 +0300

    added homework from lessons 1 and 2

---
+++ HW_1/pythonLesson1.py
@@ -0,0 +1,54 @@
+# Homework 1
+# Task 1:
+# "Створіть дві змінні first=10, second=30.
+# Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел"
+
+first = 10
+second = 30
+
+result_addition = first + second
+print(result_addition)
+
+result_subtraction = first - second
+print(result_subtraction)
+
+result_multiplication = first * second
+print(result_multiplication)
+
+result_division = first / second
+print(result_division)
+
+result_int = first // second
+print(result_int)
+
+result_rest = first % second
+print(result_rest)
+
+result_exponentiation = first ** second
+print(result_exponentiation)
+
+# Task 2:
+# "Створіть змінну і запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+# Виведіть на екран результат кожного порівняння."
+
+compare_result = first > second
+print(compare_result)
+
+compare_result = first < second
+print(compare_result)
+
+compare_result = first == second
+print(compare_result)
+
+compare_result = first != second
+print(compare_result)
+
+# Task 3:
+# "Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world".
+# Виведіть на екран."
+
+str1 = "Hello "
+str2 = "world"
+
+text = str1 + str2
+print(text)
